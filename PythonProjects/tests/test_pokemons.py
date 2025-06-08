f6c1963a6e7fe402f65550c8574d527297e42872
Mariya Lipilina <lipilina.mariya@yandex.ru>
2025-06-08 23:08:49 +0300

    Update test_pokemons.py

--- PythonProjects/tests/test_pokemons.py
+++ PythonProjects/tests/test_pokemons.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL= 'https://api.pokemonbattle.ru/v2'
-TOKEN= '94e34472e3ff3042981a55aa0df85b45'
+TOKEN= 'Введите свой токен'
 HEADR= {'Content-Type':'application/json','trainer_token':TOKEN}
 TRAINER_ID = '33372'
 
